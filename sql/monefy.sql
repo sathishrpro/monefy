d146562a1aab8e7085a696e92c1277ce023101c5
Sathish <sathish@chennovate.com>
2019-04-10 11:21:10 +0530

    Test db sql

--- sql/monefy.sql
+++ sql/monefy.sql
@@ -1,256 +1,119 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 07, 2019 at 07:39 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.1.13
+/* Database export results for db monefy */
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+/* Preserve session variables */
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS;
+SET FOREIGN_KEY_CHECKS=0;
 
+/* Export data */
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `monefy`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `budget`
---
-
+/* Table structure for budget */
 CREATE TABLE `budget` (
-  `budget_id` int(11) NOT NULL,
+  `budget_id` int(11) NOT NULL AUTO_INCREMENT,
   `amount` decimal(10,2) NOT NULL,
-  `category_id` int(11) UNSIGNED DEFAULT NULL,
-  `user_id` int(11) UNSIGNED DEFAULT NULL,
+  `category_id` int(11) unsigned DEFAULT NULL,
+  `user_id` int(11) unsigned DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`budget_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+/* data for Table budget */
+INSERT INTO `budget` VALUES (1,"9300.00",1,1,"2019-04-10 08:43:41","2019-04-10 08:43:41");
+INSERT INTO `budget` VALUES (2,"10000.00",7,1,"2019-04-10 08:44:07","2019-04-10 08:44:07");
+INSERT INTO `budget` VALUES (3,"10000.00",2,1,"2019-04-10 08:44:25","2019-04-10 08:44:25");
+INSERT INTO `budget` VALUES (4,"1300.00",4,1,"2019-04-10 08:44:57","2019-04-10 08:44:57");
+INSERT INTO `budget` VALUES (5,"4000.00",3,1,"2019-04-10 08:48:40","2019-04-10 08:48:40");
+INSERT INTO `budget` VALUES (6,"2000.00",5,1,"2019-04-10 08:48:54","2019-04-10 08:48:54");
+INSERT INTO `budget` VALUES (7,"3000.00",6,1,"2019-04-10 08:49:03","2019-04-10 08:49:03");
+INSERT INTO `budget` VALUES (8,"1000.00",8,1,"2019-04-10 09:08:44","2019-04-10 09:08:44");
+INSERT INTO `budget` VALUES (9,"1000.00",9,1,"2019-04-10 09:08:53","2019-04-10 09:08:53");
 
---
--- Table structure for table `expense`
---
-
+/* Table structure for expense */
 CREATE TABLE `expense` (
-  `expense_id` int(11) UNSIGNED NOT NULL,
+  `expense_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `amount` decimal(10,2) NOT NULL,
   `category_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `expense_date` date NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `recurring_cost_type_id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `recurring_cost_type_id` int(5) NOT NULL,
+  PRIMARY KEY (`expense_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+/* data for Table expense */
+INSERT INTO `expense` VALUES (1,"300.00",1,1,"2019-01-01","2019-04-10 08:41:33","2019-04-10 08:41:33",2);
+INSERT INTO `expense` VALUES (2,"6000.00",7,1,"2019-01-03","2019-04-10 08:43:05","2019-04-10 08:43:05",1);
 
---
--- Table structure for table `expense_categories`
---
-
+/* Table structure for expense_categories */
 CREATE TABLE `expense_categories` (
-  `expense_category_id` int(11) NOT NULL,
-  `expense_category` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `expense_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `expense_category` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`expense_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `expense_categories`
---
+/* data for Table expense_categories */
+INSERT INTO `expense_categories` VALUES (1,"Food");
+INSERT INTO `expense_categories` VALUES (2,"Home");
+INSERT INTO `expense_categories` VALUES (3,"Shopping");
+INSERT INTO `expense_categories` VALUES (4,"Transport");
+INSERT INTO `expense_categories` VALUES (5,"Healthcare");
+INSERT INTO `expense_categories` VALUES (6,"Education");
+INSERT INTO `expense_categories` VALUES (7,"Bills & Fees");
+INSERT INTO `expense_categories` VALUES (8,"Entertainment");
+INSERT INTO `expense_categories` VALUES (9,"Gifts");
+INSERT INTO `expense_categories` VALUES (10,"Travel");
 
-INSERT INTO `expense_categories` (`expense_category_id`, `expense_category`) VALUES
-(1, 'Food'),
-(2, 'Home'),
-(3, 'Shopping'),
-(4, 'Transport'),
-(5, 'Healthcare'),
-(6, 'Education'),
-(7, 'Bills & Fees'),
-(8, 'Entertainment'),
-(9, 'Gifts'),
-(10, 'Travel');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `income`
---
-
+/* Table structure for income */
 CREATE TABLE `income` (
-  `income_id` int(11) UNSIGNED NOT NULL,
+  `income_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `amount` decimal(10,2) NOT NULL,
   `category_id` int(11) NOT NULL,
   `income_date` date NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`income_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+/* data for Table income */
+INSERT INTO `income` VALUES (1,"45000.00",1,"2019-01-01","2019-04-10 08:37:10","2019-04-10 08:37:10",1);
 
---
--- Table structure for table `income_categories`
---
-
+/* Table structure for income_categories */
 CREATE TABLE `income_categories` (
-  `income_category_id` int(11) NOT NULL,
-  `income_category` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `income_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `income_category` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`income_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `income_categories`
---
+/* data for Table income_categories */
+INSERT INTO `income_categories` VALUES (1,"Salary");
+INSERT INTO `income_categories` VALUES (2,"Business");
 
-INSERT INTO `income_categories` (`income_category_id`, `income_category`) VALUES
-(1, 'Salary'),
-(2, 'Business');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `recurring_costs_type`
---
-
+/* Table structure for recurring_costs_type */
 CREATE TABLE `recurring_costs_type` (
-  `recurring_cost_type_id` int(11) UNSIGNED NOT NULL,
-  `recurring_cost_type` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `recurring_cost_type_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recurring_cost_type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`recurring_cost_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `recurring_costs_type`
---
+/* data for Table recurring_costs_type */
+INSERT INTO `recurring_costs_type` VALUES (1,"Once");
+INSERT INTO `recurring_costs_type` VALUES (2,"Daily");
+INSERT INTO `recurring_costs_type` VALUES (3,"Weekly");
 
-INSERT INTO `recurring_costs_type` (`recurring_cost_type_id`, `recurring_cost_type`) VALUES
-(1, 'Once'),
-(2, 'Daily'),
-(3, 'Weekly');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+/* Table structure for users */
 CREATE TABLE `users` (
-  `user_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
+/* data for Table users */
+INSERT INTO `users` VALUES (1,"sathish","0bbd64a3917b62ac50a4b8ca225438115f497f8b","2019-04-10 08:29:47","2019-04-10 08:29:47");
 
-INSERT INTO `users` (`user_id`, `user_name`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'sathish', '1199a6795357218a1bee6d175498675d92850c2a', NULL, NULL),
-(3, 'sathish1', '1199a6795357218a1bee6d175498675d92850c2a', NULL, NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `budget`
---
-ALTER TABLE `budget`
-  ADD PRIMARY KEY (`budget_id`);
-
---
--- Indexes for table `expense`
---
-ALTER TABLE `expense`
-  ADD PRIMARY KEY (`expense_id`);
-
---
--- Indexes for table `expense_categories`
---
-ALTER TABLE `expense_categories`
-  ADD PRIMARY KEY (`expense_category_id`);
-
---
--- Indexes for table `income`
---
-ALTER TABLE `income`
-  ADD PRIMARY KEY (`income_id`);
-
---
--- Indexes for table `income_categories`
---
-ALTER TABLE `income_categories`
-  ADD PRIMARY KEY (`income_category_id`);
-
---
--- Indexes for table `recurring_costs_type`
---
-ALTER TABLE `recurring_costs_type`
-  ADD PRIMARY KEY (`recurring_cost_type_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `budget`
---
-ALTER TABLE `budget`
-  MODIFY `budget_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `expense`
---
-ALTER TABLE `expense`
-  MODIFY `expense_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `expense_categories`
---
-ALTER TABLE `expense_categories`
-  MODIFY `expense_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `income`
---
-ALTER TABLE `income`
-  MODIFY `income_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `income_categories`
---
-ALTER TABLE `income_categories`
-  MODIFY `income_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `recurring_costs_type`
---
-ALTER TABLE `recurring_costs_type`
-  MODIFY `recurring_cost_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/* Restore session variables to original values */
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
